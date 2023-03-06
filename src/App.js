35d61133f29e1545125362191128220ef9ab4763
Nicolas Pieroni Aranda <nicolaspieroniaranda@gmail.com>
2023-03-06 17:21:25 -0300

    pokemon-app#6 done, conected to poke api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello</div>;
+  const pokeApiUrl = "https://pokeapi.co/api/v2";
+
+  fetch(`${pokeApiUrl}/ability/1`)
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.log(error));
+
+  return <div></div>;
 }
 
 export default App;
