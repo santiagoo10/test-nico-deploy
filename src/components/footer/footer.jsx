2ad56313db8d44a551930ba1e0b8b4cfa2d58ada
Nicolas Pieroni Aranda <nicolaspieroniaranda@gmail.com>
2023-03-20 11:45:19 -0300

    pokemon-app#26 done

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,4 +1,5 @@
 import "./footer.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export function Footer() {
   return (
@@ -7,16 +8,22 @@ export function Footer() {
 
       <ul>
         <li>
-          <img src="/images/instagram.svg" alt="Instagram" />
+          <a href='https://www.instagram.com/pokemon/' target='_blank' rel='noreferrer noopener'><img src="/images/instagram.svg" alt="Instagram" /></a>
         </li>
         <li>
-          <img src="/images/tiktok.svg" alt="Tik Tok" />
+          <a href='https://www.tiktok.com/@pokemonofficial?lang=es' target='_blank' rel='noreferrer noopener'><img src="/images/tiktok.svg" alt="Tik Tok" /></a>
         </li>
         <li>
-          <img src="/images/twitter.svg" alt="Twitter" />
+          <a href='https://twitter.com/Pokemon' target='_blank' rel='noreferrer noopener'><img src="/images/twitter.svg" alt="Twitter" /></a>
         </li>
         <li>
-          <img src="/images/discord.svg" alt="Discord" />
+          <a href='https://discord.com/invite/pokemon' target='_blank' rel='noreferrer noopener'><img src="/images/discord.svg" alt="Discord" /></a>
+        </li>
+        <li>
+          <a href='https://www.youtube.com/channel/UCFctpiB_Hnlk3ejWfHqSm6Q' target='_blank' rel='noreferrer noopener'><img src="/images/youtube.svg" alt="YouTube" /></a>
+        </li>
+        <li>
+          <a href='https://www.twitch.tv/pokemon' target='_blank' rel='noreferrer noopener'><img src="/images/twitch.svg" alt="Twitch" /></a>
         </li>
       </ul>
     </footer>
