bb75a44112b83e78939104b2be41c5e60d01af97
Nicolas Pieroni Aranda <nicolaspieroniaranda@gmail.com>
2023-03-21 15:38:48 -0300

    pokemon-app#30, done

--- src/components/searchFilters/searchFilters.jsx
+++ src/components/searchFilters/searchFilters.jsx
@@ -1,10 +1,21 @@
 import "./searchFilters.css"
+//import { useState } from "react";
+//import { useFetchGetAllPokemons } from "../../useFetch/getAllPokemons";
 
-export function SearchFilters() {
+export function SearchFilters({ handleSearchBar }) {
+    // const [searchBar, setSearchBar] = useState('')
+    // const { pokemons } = useFetchGetAllPokemons()
+
+    // function handleSearchBar(e) {
+    //     setSearchBar(e.target.value)
+    //     pokemons.filter((pokemon) => pokemon.name.toLowerCase().includes(searchBar))
+
+    // }
+
     return (
         <div className="inputsContainer">
             <label className="searchBar">
-                <input name="searchBar" placeholder="Pikachu" />
+                <input name="searchBar" placeholder="Pikachu, Dito" onInput={handleSearchBar} />
                 <img src="/images/lupa.png" alt="magnifying glass" />
             </label>
 
