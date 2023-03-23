25ed78e4b375db68992e6ebfd42c566f51383ffc
Nicolas Pieroni Aranda <nicolaspieroniaranda@gmail.com>
2023-03-23 14:18:52 -0300

    filter select by A-Z, done

---
+++ src/components/cards/selectFilterCards.jsx
@@ -0,0 +1,48 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+
+import { generateNewColor } from "../../randomColor/randomColor";
+import { randomPrice } from "../../randomPrice/randomPrice";
+
+export function SelectFilterCards({ selectFilterResults }) {
+
+    return (
+        <>
+            {selectFilterResults?.map((pokemon) => (
+                <Card
+                    style={{
+                        width: "160px",
+                        backgroundColor: "rgb(39, 43, 44)",
+                        color: "white",
+                        borderRadius: "10px",
+                    }}
+                    key={pokemon.id}
+                >
+                    <Card.Img
+                        variant="top"
+                        src={pokemon.sprites.front_default}
+                        style={{
+                            width: "100%",
+                            alignSelf: "center",
+                            backgroundColor: `${generateNewColor()}`,
+                            borderRadius: "10px 10px 0 0",
+                        }}
+                    />
+                    <Card.Body
+                        style={{
+                            display: "flex",
+                            flexDirection: "column",
+                            rowGap: "10px",
+                        }}
+                    >
+                        <Card.Title style={{ margin: "0" }}>{pokemon.name}</Card.Title>
+                        <Card.Subtitle>{`U$$ ${randomPrice()}`}</Card.Subtitle>
+                        <Button variant="primary">View More</Button>
+                        <Button variant="warning">Buy Now</Button>
+                    </Card.Body>
+                </Card>
+            ))}
+        </>
+    )
+}
--- src/components/searchFilters/searchFilters.jsx
+++ src/components/searchFilters/searchFilters.jsx
@@ -1,6 +1,6 @@
 import "./searchFilters.css"
 
-export function SearchFilters({ handleSearchBar }) {
+export function SearchFilters({ handleSearchBar, handleAZfilterSelect }) {
 
     return (
         <div className="inputsContainer">
@@ -10,10 +10,12 @@ export function SearchFilters({ handleSearchBar }) {
             </label>
 
             <label className="filter">
-                <select name="filter">
-                    <option>A-Z</option>
-                    <option>Color</option>
+                <select name="filter" onChange={handleAZfilterSelect}>
+                    <option value="none" disabled selected>Filter by...</option>
+                    <option value="a-z">A-Z</option>
+                    <option value="more expensive to cheaper">More expensive to cheaper</option>
+                    <option value="cheaper to more expensive">Cheaper to more expensive</option>
                 </select>
             </label>
-        </div>)
+        </div >)
 }
